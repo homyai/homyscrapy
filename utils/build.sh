94a6f4aacc96ef10121e6542f47e0d33685e5312
Nicolás <nicomedrano@gmail.com>
2024-02-14 20:04:00 -0600

    Add utils

---
+++ utils/build.sh
@@ -0,0 +1,12 @@
+# Building the local image. From the root directory of the repo, run:
+# For Intel architecture:
+
+# docker build --no-cache --platform linux/amd64 -t policyconfluence/polly:latest -f .devcontainer/Dockerfile .
+
+# or for Mac Silicon architecture:
+
+docker build --platform linux/arm64 -t nicomedrano/homyscrapy:v0.1 -f ./Dockerfile .
+
+# Note that you need to build the image for the right architecture. If you get the
+# message "exec format error", it means you built it for the wrong architecture.
+
