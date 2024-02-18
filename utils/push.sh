70fd080ee2df4fe93cb7e298978c5c0a109aabb5
Nicolás <nicomedrano@gmail.com>
2024-02-17 18:27:58 -0600

    Final run

--- utils/push.sh
+++ utils/push.sh
@@ -1,1 +1,1 @@
-docker login && docker push nicomedrano/homyscrapy:v0.1 && docker build -t nicomedrano/homyscrapy:latest . && docker push nicomedrano/homyscrapy:latest
+docker login && docker push nicomedrano/homyscrapy:v0.2 && docker build -t nicomedrano/homyscrapy:latest . && docker push nicomedrano/homyscrapy:latest
