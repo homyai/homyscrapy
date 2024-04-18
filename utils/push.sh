1dc65292b843f822737eef7b77a02b7fa6267b84
Nicolás <nicomedrano@gmail.com>
2024-04-18 10:14:49 -0600

    Try this

--- utils/push.sh
+++ utils/push.sh
@@ -1,1 +1,1 @@
-docker login && docker push nicomedrano/homyscrapy:v0.4 && docker build -t nicomedrano/homyscrapy:latest . && docker push nicomedrano/homyscrapy:latest
+docker login && docker push nicomedrano/homyscrapy:v0.5 && docker build -t nicomedrano/homyscrapy:latest . && docker push nicomedrano/homyscrapy:latest
