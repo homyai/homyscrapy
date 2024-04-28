2b91ce7429347ace394d803849267a3c0ba2d0a1
Nicolás <nicomedrano@gmail.com>
2024-04-28 17:11:54 +0000

    Refactor name

--- common/scraper/scraper.py
+++ common/scraper/scraper.py
@@ -3,7 +3,7 @@ import pandas as pd
 import json
 import time
 
-class ScrapTool():
+class Scraper():
     """
         Class used for BS4 Scrapping, that contains normal tools for reading elements
         suchs as tables, <ul>,<dl>, urls, etc...
@@ -13,20 +13,18 @@ class ScrapTool():
             Init class with soup response
         """
         self.response = response
-    def soup_creation(self):
-        soup = BeautifulSoup(self.response.text,'lxml')
-        return soup
+        self.soup = BeautifulSoup(self.response.text,'lxml')
 
-    def table_read(self, sopa, table_class = None):
+    def table_read(self, table_class = None):
         """
             Read whole table
         """
         try:
             if table_class==None:
                 #find table
-                table = sopa.find('table')
+                table = self.soup.find('table')
             else:
-                table = sopa.find('table',{'class':table_class})
+                table = self.soup.find('table',{'class':table_class})
             #find table body 
             tbody = table.find('tbody')
 
@@ -44,12 +42,12 @@ class ScrapTool():
             data_dict = {}
         return data_dict
 
-    def table_read_col(self,sopa, table_col,tag,extract):
+    def table_read_col(self, table_col,tag,extract):
         """
             Return all values from a single colum
         """
         #find table
-        table = sopa.find('table')
+        table = self.soup.find('table')
         #find table body 
         rows = table.find('tbody')
         desired_col = rows.find_all('td', {'class':table_col})
@@ -59,14 +57,14 @@ class ScrapTool():
             url_list.append(url)
         return url_list
 
-    def dl_read(self,sopa, dl_class=None):
+    def dl_read(self, dl_class=None):
         """
             Read Data List from HTML
         """
         col_names = []
         values = []
         if dl_class==None:
-            data_list = sopa.find_all('dl')
+            data_list = self.soup.find_all('dl')
             for dl in data_list:
                 data_names = dl.find_all('dt')
                 data_values = dl.find_all('dd')
@@ -77,7 +75,7 @@ class ScrapTool():
             zip_it = zip(col_names,values)
             data_dict = dict(zip_it)
         else:
-            data_list = sopa.find_all('dl', {'class':dl_class})
+            data_list = self.soup.find_all('dl', {'class':dl_class})
             for dl in data_list:
                 data_names = dl.find_all('dt')
                 data_values = dl.find_all('dd')
@@ -89,31 +87,32 @@ class ScrapTool():
             data_dict = dict(zip_it)
         return data_dict
 
-    def search_nest(self, sopa, str_dict):
+    def search_nest(self, str_dict, soup=None):
         """
             Nested search for any kind of element.
         """
+        soup = self.soup if soup is None else soup
         try:
             for step in str_dict:
                 if(str_dict[step]['function']=="find"):
-                    response = sopa.find(str_dict[step]['search'],
+                    response = soup.find(str_dict[step]['search'],
                                         {   str_dict[step]['key']:
                                             str_dict[step]['key_name']
                                         })
                 else:
-                    response = sopa.find_all(str_dict[step]['search'],
+                    response = soup.find_all(str_dict[step]['search'],
                         {   str_dict[step]['key']:
                             str_dict[step]['key_name']
                         })
-                sopa = response
+                new_soup = response
         except:
             for step in str_dict:
                 if(str_dict[step]['function']=="find"):
-                    response = sopa.find(str_dict[step]['search'])
+                    response = soup.find(str_dict[step]['search'])
                 else:
-                    response = sopa.find_all(str_dict[step]['search'])
-                sopa = response
-        return(sopa)
+                    response = soup.find_all(str_dict[step]['search'])
+                new_soup = response
+        return new_soup
     
     def getatr_fromlist(self,list_,tag,attr):
         """
@@ -136,13 +135,28 @@ class ScrapTool():
             except:
                 return list_.find(tag).get(attr)
 
-    def next_page(self, sopa , nav_class, ul_class):
+    def next_page(self, nav_class, ul_class):
         """
             Find next url of an interation page
         """
-        div_pages = sopa.find('div', {'class' : nav_class})
+        div_pages = self.soup.find('div', {'class' : nav_class})
         ul_item = div_pages.find('ul',{'class' : ul_class})
         last_li = ul_item.find_all('li')[-1].find('a').get('href')
         text_last_li =ul_item.find_all('li')[-1].find('a').get_text()
         return last_li,text_last_li
-    
+
+    def scrape_urls_from_properties_page(self, steps:json) -> list:
+        """
+        Scrapes the urls from the properties page.
+        """
+        urls_list = []
+        urls_divs = self.search_nest(steps["P1"]) # Gets the section that contains the posts in the Propertie Page
+        for link in urls_divs:
+            url = (
+                (self.search_nest(steps["P2"], link)) # Gets the link of the post
+                .find("h2")
+                .find("a")
+                .get("href")
+            )
+            urls_list.append(url) # Appends the link to the list of links
+        return urls_list
